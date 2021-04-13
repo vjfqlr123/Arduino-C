75b1684e0b60b0d17160969a258b522d442bc931
vjfqlr123 <82442705+vjfqlr123@users.noreply.github.com>
2021-04-13 19:19:38 +0900

    Add files via upload

---
+++ 13_minireed_sw.ino
@@ -0,0 +1,34 @@
+#define vcc 3
+#define gnd 4
+#define sig 2
+
+boolean sig_state = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  pinMode(vcc, OUTPUT);
+  pinMode(gnd, OUTPUT);
+  pinMode(sig, INPUT);
+
+  digitalWrite(vcc, HIGH);
+  digitalWrite(gnd, LOW);
+
+  attachInterrupt(digitalPinToInterrupt(sig), tilt_state_print, CHANGE);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
+
+void tilt_state_print() {
+  if(!digitalRead(sig))       // Mini reed OFF: high, ON: low
+  {
+    Serial.println("Mini reed ON");      // LOW
+  }else {
+    Serial.println("Mini reed OFF");     // OFF
+  }
+}
